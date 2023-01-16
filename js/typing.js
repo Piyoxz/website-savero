aba01e68ae631a9fcdfc74ac6cfd486e5c3128e5
Piyoxz <102013178+Piyoxz@users.noreply.github.com>
2023-01-16 08:03:21 +0700

    Add files via upload

---
+++ js/typing.js
@@ -0,0 +1,11 @@
+var typed = new Typed(".teks-berjalan", {
+    strings: [
+        'Digital',
+        'Network',
+        'Marketing'
+      ],
+    loop: true,
+    typeSpeed: 50,
+    backSpeed: 25,
+    backDelay: 500,
+  });
